5c28697cef55f6476cadc5bd0d5ab657e00cd587
nada ali <nadaelhabashy19@gmail.com>
2024-08-14 19:07:40 +0300

    update

--- src/app/browse-categories/browse-categories.component.ts
+++ src/app/browse-categories/browse-categories.component.ts
@@ -1,12 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-browse-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './browse-categories.component.html',
-  styleUrl: './browse-categories.component.css'
+  styleUrl: './browse-categories.component.css',
 })
 export class BrowseCategoriesComponent {
-
+  categories = [
+    {
+      id: 1,
+      name: 'Art',
+      src: 'Category Icon.png',
+      alt: 'Image 1',
+    },
+    {
+      id: 2,
+      name: 'Collectibles',
+      src: 'Category Icon2.png',
+      alt: 'Image 2',
+    },
+    {
+      id: 3,
+      name: 'Music',
+      src: 'Category Icon3.png',
+      alt: 'Image 3',
+    },
+    {
+      id: 4,
+      name: 'Photography',
+      src: 'Category Icon4.png',
+      alt: 'Image 4',
+    },
+    {
+      id: 5,
+      name: 'Video',
+      src: 'Category Icon5.png',
+      alt: 'Image 5',
+    },
+    {
+      id: 6,
+      name: 'Utility',
+      src: 'Category Icon6.png',
+      alt: 'Image 6',
+    },
+    {
+      id: 7,
+      name: 'Sport',
+      src: 'Category Icon7.png',
+      alt: 'Image 7',
+    },
+    {
+      id: 8,
+      name: 'Virtual Worlds',
+      src: 'Category Icon8.png',
+      alt: 'Image 8',
+    },
+  ];
 }
--- src/app/top-rated-artist/top-rated-artist.component.ts
+++ src/app/top-rated-artist/top-rated-artist.component.ts
@@ -1,12 +1,98 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-rated-artist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './top-rated-artist.component.html',
-  styleUrl: './top-rated-artist.component.css'
+  styleUrl: './top-rated-artist.component.css',
 })
 export class TopRatedArtistComponent {
-
+  topRated = [
+    {
+      id: 1,
+      name: 'Keepitreal',
+      src: 'c1.png',
+      alt: 'Image 1',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 2,
+      name: 'DigiLab',
+      src: 'c2.png',
+      alt: 'Image 2',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 3,
+      name: 'GravityOne',
+      src: 'c3.png',
+      alt: 'Image 3',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 4,
+      name: 'Juanie',
+      src: 'c4.png',
+      alt: 'Image 4',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 5,
+      name: 'BlueWhale',
+      src: 'c5.png',
+      alt: 'Image 5',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 6,
+      name: 'mr fox',
+      src: 'c6.png',
+      alt: 'Image 6',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 7,
+      name: 'Shroomie',
+      src: 'c7.png',
+      alt: 'Image 7',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 8,
+      name: 'robotica',
+      src: 'c8.png',
+      alt: 'Image 8',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 9,
+      name: 'RustyRobot',
+      src: 'c9.png',
+      alt: 'Image 9',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 10,
+      name: 'animakid',
+      src: 'c10.png',
+      alt: 'Image 10',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 11,
+      name: 'Dotgu',
+      src: 'c11.png',
+      alt: 'Image 11',
+      totalSales: '34.53 ETH',
+    },
+    {
+      id: 12,
+      name: 'Ghiblier',
+      src: 'c12.png',
+      alt: 'Image 12',
+      totalSales: '34.53 ETH',
+    },
+  ];
 }
