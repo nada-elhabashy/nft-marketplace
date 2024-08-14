05e286590724aa04c6ae445c2e48da42fb6eb021
nada ali <nadaelhabashy19@gmail.com>
2024-08-14 20:13:54 +0300

    modification

--- src/app/discover-more/discover-more.component.ts
+++ src/app/discover-more/discover-more.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-discover-more',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './discover-more.component.html',
-  styleUrl: './discover-more.component.css'
+  styleUrl: './discover-more.component.css',
 })
 export class DiscoverMoreComponent {
-
+  discover = [
+    {
+      id: 1,
+      name: 'Distant Galaxy',
+      cotent: 'MoonDancer',
+      src: 'discover1.png',
+      alt: 'Image 1',
+      price: '1.63 ETH',
+      bid: '0.33 wETH',
+    },
+    {
+      id: 2,
+      name: 'Life On Edena',
+      cotent: 'NebulaKid',
+      src: 'discover2.png',
+      alt: 'Image 1',
+      price: '1.63 ETH',
+      bid: '0.33 wETH',
+    },
+    {
+      id: 3,
+      name: 'AstroFiction',
+      cotent: 'MoonDancer',
+      src: 'discover3.png',
+      alt: 'Image 1',
+      price: '1.63 ETH',
+      bid: '0.33 wETH',
+    },
+  ];
 }
--- src/app/trending-collection/trending-collection.component.ts
+++ src/app/trending-collection/trending-collection.component.ts
@@ -1,18 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-trending-collection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './trending-collection.component.html',
-  styleUrl: './trending-collection.component.css'
+  styleUrl: './trending-collection.component.css',
 })
 export class TrendingCollectionComponent {
-  imageArray = [
-    { src: 'c1.PNG', alt: 'Image 1' },
-    { src: 'c2.PNG', alt: 'Image 2' },
-    { src: 'c3.PNG', alt: 'Image 3' },
-     { src: 'c4.PNG', alt: 'Image 4' }
-
+  trending = [
+    {
+      id: 1,
+      name: 'DSGN Animals',
+      cotent: 'MoonDancer',
+      src: 'Primary Photo Placeholder.png',
+      alt: 'Image 1',
+      img1: 'Secondary Photo Placeholder.png',
+      img2: 'Secondary Photo Placeholder1.png',
+    },
+    {
+      id: 2,
+      name: 'Magic Mushrooms',
+      cotent: 'NebulaKid',
+      src: 'Primary Photo Placeholder2.png',
+      alt: 'Image 1',
+      img1: 'Secondary Photo Placeholder2.png',
+      img2: 'Secondary Photo Placeholder22.png',
+    },
+    {
+      id: 3,
+      name: 'Disco Machines',
+      cotent: 'MoonDancer',
+      src: 'Primary Photo Placeholder3.png',
+      alt: 'Image 1',
+      img1: 'Secondary Photo Placeholder3.png',
+      img2: 'Secondary Photo Placeholder33.png',
+    },
   ];
 }
